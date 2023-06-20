49ae91ed92d7dfc6f90f9008fdcab361aa6a3d69
Yuga <Trev@brennberry.com>
2023-06-20 16:46:26 -0400

    copied my code prior try. gave up. made new!

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3003;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.json(JSON.parse(data));
+    }
+  });
+});
+
+app.post('/api/notes', (req, res) => {
+  fs.readFile('./db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const notes = JSON.parse(data);
+      const newNote = req.body;
+      newNote.id = Date.now().toString(); // Generate a unique id for each note
+      notes.push(newNote);
+
+      fs.writeFile('./db.json', JSON.stringify(notes, null, 2), (writeErr) => {
+        if (writeErr) {
+          console.error(writeErr);
+        } else {
+          res.status(200).json(newNote);
+        }
+      });
+    }
+  });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  fs.readFile('./db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      let notes = JSON.parse(data);
+      notes = notes.filter((note) => note.id !== req.params.id);
+
+      fs.writeFile('./db.json', JSON.stringify(notes, null, 2), (writeErr) => {
+        if (writeErr) {
+          console.error(writeErr);
+        } else {
+          res.sendStatus(200);
+        }
+      });
+    }
+  });
+});
+
+app.listen(PORT, () => console.log(`Server is listening on PORT ${PORT}`));
